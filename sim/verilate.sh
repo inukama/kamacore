ededf005c1bc8bfe67cff861ce184b14af61cd4d
inukama <60096629+inukama@users.noreply.github.com>
2025-09-05 14:56:10 +0930

    feat: Makefile

--- sim/verilate.sh
+++
@@ -1,6 +0,0 @@
-echo VERILATING 1
-verilator --cc ../src/kamacore_top.sv --relative-includes --top-module kamacore_top
-echo VERILATING 2
-verilator -Wall --trace -cc ../src/kamacore_top.sv -exe tb_kamacore_top.cpp --relative-includes --top-module kamacore_top
-echo MAKE 
-make -C obj_dir/ -f Vkamacore_top.mk Vkamacore_top
